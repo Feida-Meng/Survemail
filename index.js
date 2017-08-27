f8a369f4d711333b51a3f46a6b6162173e6cf6c0
codonculous <feidameng01@gmail.com>
2017-08-27 14:49:28 -0400

    Set up passport config for goole oauth and added get /auth/google

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({hi: 'there'});
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.GoogleOauthClientID,
+      clientSecret: keys.GoogleOauthClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+    // ask google to give permission to share profile and email
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
