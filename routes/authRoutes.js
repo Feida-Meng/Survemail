5158dd0151dc6e0ce9988327553dceaaac7a69b1
codonculous <feidameng01@gmail.com>
2017-08-28 17:58:40 -0400

    installed create-react-app

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -16,7 +16,7 @@ module.exports = app => {
     res.send(req.user);
   });
 
-  app.get('/api_current_user', (req, res) => {
+  app.get('/api/current_user', (req, res) => {
     res.send(req.user);
   });
 };
