99f5539c21e76aab49b7d10b2168343a37eb3217
codonculous <feidameng01@gmail.com>
2017-08-28 00:28:46 -0400

    Enabled cookies

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 const keys = require('./config/keys');
 require('./models/User'); //need to run this before passport
@@ -9,6 +11,16 @@ mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI, {useMongoClient: true});
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 // above is same as:
