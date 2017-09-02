98dfee5b30e8d37b4e825fbad24391669e73fcd8
codonculous <feidameng01@gmail.com>
2017-09-02 18:33:50 -0400

    Created fetch_user action creator and connnected it to App component

---
+++ client/src/actions/index.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import {FETCH_USER} from './types';
+
+export const fetchUser = () =>
+// No need for return and {} if there is only a single expression
+// within the {}
+  async (dispatch) => {
+    const res = await axios.get('/api/current_user');
+    dispatch({type: FETCH_USER, payload: res});
+  };
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,14 @@
 import React, {Component} from 'react';
 import {BrowserRouter, Route } from 'react-router-dom';
 import Header from './Header'
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
   render() {
     return (
@@ -20,4 +26,4 @@ class App extends Component {
   }
 };
 
-export default App;
+export default connect(null, actions)(App);
