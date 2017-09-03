5f02a27319395cbd71661ee44728765fecbb5b88
codonculous <feidameng01@gmail.com>
2017-09-02 22:25:44 -0400

    Made login redirect to /survey

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,8 +9,14 @@ module.exports = app => {
     })
   );
 
-  app.get('/auth/google/callback', passport.authenticate('google'));
- 
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/surveys');
+    }
+  );
+
   app.get('/api/logout', (req, res) => {
     req.logout(); // logout is added by passport
     res.send(req.user);
