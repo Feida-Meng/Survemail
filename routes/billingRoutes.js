89c428d9581fab2bb66d71ba32b0c1b0107dabf3
codonculous <feidameng01@gmail.com>
2017-09-04 18:37:28 -0400

    Added catch error for billingRoutes

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -4,16 +4,26 @@ const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
   app.post('/api/stripe', requireLogin, async (req, res) => {
-    const charge = await stripe.charges.create({
-      amount: 500,
-      currency: "usd",
-      source: req.body.id,
-      description: `Charged ${req.body.card.name} $5 for 5 credits`
-    });
+
+    try{
+      const charge = await stripe.charges.create({
+        amount: 500,
+        currency: "usd",
+        source: req.body.id,
+        description: `Charged ${req.body.card.name} $5 for 5 credits`
+      });
+    } catch (e) {
+      return res.status(400).send(e);
+    }
 
     req.user.credits += 5;
-    const user = await req.user.save();
-    res.send(user);
+    
+    try {
+      const user = await req.user.save();
+      res.send(user);
+    } catch (e) {
+      res.status(400).send(e);
+    }
 
   });
 }
