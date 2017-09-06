0aa0fa94abac4e4fe59ab15e1e108624624362b5
codonculous <feidameng01@gmail.com>
2017-09-06 00:04:18 -0400

    Completed post /api/survey route handler to allow email to be sent and survey to be saved

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,7 +7,7 @@ const surveyTemplate = require('../services/emailTemplate/surveyTemplate');
 
 module.exports = app => {
 
-  app.post('/api/survey', requireLogin, requireCredit, (req, res) => {
+  app.post('/api/survey', requireLogin, requireCredit, async (req, res) => {
     const {title, subject, body, recipients} = req.body;
     const survey = new Survey({
       title,
@@ -17,9 +17,19 @@ module.exports = app => {
       _userID: req.user.id,
       dataSend: Date.now()
     });
-    // Send email
-    const mailer = new Mailer(survey,surveyTemplate(survey));
-    survey.save()
+
+    try {
+      //create email
+      const mailer = new Mailer(survey,surveyTemplate(survey));
+      // Send email
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+      res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
+    }
   });
 
 }
